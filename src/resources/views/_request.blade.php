27d2d1290f2aad2daa8c43bfea57361203df4678
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2025-06-11 11:19:21 +0300

    Improved the PayPal request snippet

--- src/resources/views/_request.blade.php
+++ src/resources/views/_request.blade.php
@@ -1,12 +1,12 @@
-<form method="post" action="{{ $url }}" name="paypal" target="_self">
-    @if($autoRedirect)
-        <p>{{ __('You will be redirected to the secure payment page') }}</p>
-        <p>
-            <img src="{{ $url }}" alt="" title=""
-                 onload="javascript:document.paypal.submit()">
-        </p>
-    @endif
-        <button type="submit">
-            {{ __('Proceed to Payment') }}
-        </button>
-</form>
+@if($autoRedirect)
+    <p style="margin: 1rem 0">{{ $infoMessage ?? __('Now you will be redirected to the secure PayPal payment page to complete the purchase') }}</p>
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            window.location = '{{ $url }}';
+        });
+    </script>
+@endif
+<div style="margin: 1rem 0">
+    <a href="{{ $url }}">{{ $btnText ?? __('Proceed to the Payment') }}</a>
+</div>
+
